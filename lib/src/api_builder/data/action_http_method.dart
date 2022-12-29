51b83862e02cb9332ed6806a2ad6ecbf10d188e9
Gleb Batykov <gleb.batykov@gmail.com>
2022-12-30 00:47:16 +0300

    Adds DELETE http method

--- lib/src/api_builder/data/action_http_method.dart
+++ lib/src/api_builder/data/action_http_method.dart
@@ -1,3 +1,3 @@
 part of minerva_controller_generator;
 
-enum ActionHttpMethod { get, post, head, options, patch, put, trace }
+enum ActionHttpMethod { get, post, head, options, patch, put, trace, delete }
